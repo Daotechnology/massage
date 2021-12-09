ab13e461864782089033083123ab9acb6b59a5c6
Olajide <daotechnology18@gmail.com>
2021-12-09 06:07:19 +0100

    New Commit

--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express();
 
 // app.use(express.static(path.join(__dirname , 'public')));
-app.use(express.static('Public/verifiedmasseur'));
+app.use(express.static('Public/web'));
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req,res) => {
     res.send('Its working');
-})
-
+});
 
 app.listen('3000',()=>{
     console.log('Port Listening on 3000');
